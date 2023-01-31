710e360b968ddb781a91e7ed11e1c502c6826ff1
PetrTolstov <Petr.Tolstov@ivkhk.ee>
2023-01-31 18:20:33 +0200

    l

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send({message : "hi"})
+})
+
+app.post('/reg', (req, res) => {
+   console.log(req.body)
+   res.send({'status' : 'ok'})
 })
 
 app.listen(3000, () => {
