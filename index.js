64836873ebe2481ab11049dc97cbb48375dcad90
PetrTolstov <Petr.Tolstov@ivkhk.ee>
2023-02-08 18:21:20 +0200

    l

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
-const express = require("express")
-const app = express()
-const bodyParser = require("body-parser")
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const dbserices = require("./db.js");
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+const db = dbserices.createDbConnection();
 
-app.get('/', (req, res) => {
-    res.send({message : "hi"})
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.post('/reg', (req, res) => {
-   console.log(req.body)
-   res.send({'status' : 'ok'})
-})
+app.get("/", (req, res) => {
+    res.send({ message: "hi" });
+});
+
+function selectRows() {
+    db.each(`SELECT * FROM users`, (error, row) => {
+      if (error) {
+        throw new Error(error.message);
+      }
+      console.log(row);
+    });
+  }
+
+app.post("/reg", (req, res) => {
+    db.run(
+        `INSERT INTO users (name, email, password) VALUES (?, ?, ?)`,
+        [req.body.name, req.body.email, req.body.password],
+        function (error) {
+            if (error) {
+                console.error(error.message);
+            }
+            console.log(`Inserted a row with the ID: ${this.lastID}`);
+            selectRows()
+        }
+    );
+    
+    res.send({ status: "ok" });
+});
+
+app.post("/log", (req, res) => {
+    console.log(req.body);
+    res.send({ status: "ok" });
+});
 
 app.listen(3000, () => {
-    console.log("server started")
-})
+    console.log("server started");
+});
